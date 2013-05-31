cfea2a021211571be2bfb4b1a334aa1851543ede
Jonas Pfenniger <jonas@pfenniger.name>
2013-05-31 21:21:18 +0100

    Use go 1.1 to compile socketmaster.
    We're seing weird issues with the shared file-descriptor. Go 1.1 creates a
    duplicate on sock.File() which might resolve issues regarding to sharing
    non-blocking file descriptors.
    
    Might be related to: https://codereview.appspot.com/6869054/

--- Vagrantfile.sh
+++ Vagrantfile.sh
@@ -2,6 +2,15 @@
 # Vagrant bootstrap script
 
 apt-get update
-DEBIAN_FRONTEND='noninteractive' apt-get install -qy git golang ruby1.9.3 build-essential
+DEBIAN_FRONTEND='noninteractive' apt-get install -qy git ruby1.9.3 build-essential
+aptitude purge -y golang golang-go golang-doc golang-src
 gem install fpm ronn -q --no-ri --no-rdoc
 
+RELEASE=go1.1.linux-amd64.tar.gz
+if ! which go >/dev/null 2>&1 ; then
+  cd /usr/local
+  rm -f $RELEASE
+  wget http://go.googlecode.com/files/$RELEASE
+  tar xzvf $RELEASE
+  echo 'export PATH="/usr/local/go/bin:$PATH"' > /etc/profile.d/golang.sh
+fi
